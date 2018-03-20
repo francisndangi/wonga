4c79b2d4a8f92fff54a1e66c23282379c55edded
unknown <francis_ndangi@yahoo.fr>
2018-03-20 17:42:08 +0200

    initial commit

---
+++ RetailDemoConsoleApp2/RetailDemoConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailDemoConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Wonga Development Test");
+            Console.WriteLine("======================\n");
+
+            Console.Write("Enter a message : ");
+
+            string myLine;
+
+            myLine = Console.ReadLine();
+
+        }
+    }
+}
